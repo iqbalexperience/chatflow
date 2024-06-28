f380693e60afe076feb9cc07183e19230331f6de
Muhammad Iqbal <82482625+iqbalexperience@users.noreply.github.com>
2024-06-28 09:07:07 -0700

    Update index.ts

--- packages/server/src/routes/openai-assistants-files/index.ts
+++ packages/server/src/routes/openai-assistants-files/index.ts
@@ -4,7 +4,24 @@ import path from 'path'
 import openaiAssistantsController from '../../controllers/openai-assistants'
 
 const router = express.Router()
-const upload = multer({ dest: `${path.join(__dirname, '..', '..', '..', 'uploads')}/` })
+// const upload = multer({ dest: `${path.join(__dirname, '..', '..', '..', 'uploads')}/` })
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, `${path.join(__dirname, '..', '..', '..', 'uploads')}/`)
+    },
+    filename: function (req, file, cb) {
+        cb(null, Math.floor(Math.random() * 1000000000) + file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+// const upload = multer({ dest: `${path.join(__dirname, '..', '..', '..', 'uploads')}/` })
+
+router.get('/', (req, res) => {
+    res.send('OpenAI Assistants Files')
+})
+
+router.post('/transcription', upload.single('file'), openaiAssistantsController.generateTranscription)
 
 router.post('/download/', openaiAssistantsController.getFileFromAssistant)
 router.post('/upload/', upload.array('files'), openaiAssistantsController.uploadAssistantFiles)
